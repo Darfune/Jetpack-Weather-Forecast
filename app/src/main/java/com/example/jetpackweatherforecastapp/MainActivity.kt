fe40823125f3dea7ea11e7f77681c202c7388f5f
Darfune <harolep@gmail.com>
2022-07-10 23:02:13 +0300

    fixed errors for dependencies

--- app/src/main/java/com/example/jetpackweatherforecastapp/MainActivity.kt
+++ app/src/main/java/com/example/jetpackweatherforecastapp/MainActivity.kt
@@ -3,34 +3,46 @@ package com.example.jetpackweatherforecastapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.jetpackweatherforecastapp.navigation.WeatherNavigation
 import com.example.jetpackweatherforecastapp.ui.theme.JetPackWeatherForecastAppTheme
 import dagger.hilt.android.AndroidEntryPoint
+import java.security.AllPermission
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JetPackWeatherForecastAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-
-                }
-            }
+            WeatherApp()
         }
     }
 }
 
+@Composable
+fun WeatherApp() {
+    JetPackWeatherForecastAppTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colors.background
+        ) {
+            Column(verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally) {
+                WeatherNavigation()
+            }
+        }
+    }
+}
 
 @Preview(showBackground = true)
 @Composable
