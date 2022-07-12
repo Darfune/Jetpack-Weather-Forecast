da7da605f22e503a1d56f5f37271c0fc0751ba49
Darfune <harolep@gmail.com>
2022-07-12 12:05:17 +0300

    Weather Api Setup

---
+++ app/src/main/java/com/example/jetpackweatherforecastapp/network/WeatherApi.kt
@@ -0,0 +1,21 @@
+package com.example.jetpackweatherforecastapp.network
+
+import com.example.jetpackweatherforecastapp.models.weather.WeatherObject
+import com.example.jetpackweatherforecastapp.utils.Constants
+import retrofit2.http.GET
+import retrofit2.http.Query
+import javax.inject.Singleton
+
+@Singleton
+interface WeatherApi {
+
+    @GET(value = "/data/2.5/forecast")
+    suspend fun getWeather(
+        @Query("lat") latitude: Double,
+        @Query("lon") longitude: Double,
+        @Query("units") units: String = "metric",
+        @Query("appid") appid: String = Constants.API_KEY
+    ) : WeatherObject
+
+
+}
