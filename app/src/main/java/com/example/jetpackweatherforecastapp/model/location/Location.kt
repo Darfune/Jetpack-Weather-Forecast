aea8cb5c1db738c0fe83ec40c142dc71c6f2dc7f
Darfune <harolep@gmail.com>
2022-07-12 10:55:35 +0300

    location object

---
+++ app/src/main/java/com/example/jetpackweatherforecastapp/model/location/Location.kt
@@ -0,0 +1,3 @@
+package com.example.jetpackweatherforecastapp.model.location
+
+class Location : ArrayList<LocationItem>()
---
+++ app/src/main/java/com/example/jetpackweatherforecastapp/model/location/LocationItem.kt
@@ -0,0 +1,9 @@
+package com.example.jetpackweatherforecastapp.model.location
+
+data class LocationItem(
+    val country: String,
+    val lat: Double,
+    val lon: Double,
+    val name: String,
+    val state: String
+)
