349d5cfc173b75cfb44c253b6f28b09ce1231f40
Darfune <harolep@gmail.com>
2022-07-13 20:33:26 +0300

    App Module with the Retrofit2 builds

--- app/src/main/java/com/example/jetpackweatherforecastapp/di/AppModule.kt
+++ app/src/main/java/com/example/jetpackweatherforecastapp/di/AppModule.kt
@@ -1,4 +1,37 @@
 package com.example.jetpackweatherforecastapp.di
 
+import com.example.jetpackweatherforecastapp.network.LocationApi
+import com.example.jetpackweatherforecastapp.network.WeatherApi
+import com.example.jetpackweatherforecastapp.utils.Constants
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
 class AppModule {
+
+    @Provides
+    @Singleton
+    fun provideOpenWeatherApi(): WeatherApi {
+        return Retrofit.Builder()
+            .baseUrl(Constants.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(WeatherApi::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideOpenWeatherLocationApi(): LocationApi {
+        return Retrofit.Builder()
+            .baseUrl(Constants.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(LocationApi::class.java)
+    }
 }
