7b5e95b295b4e593e38ed3c45bc9ef1ebd7d4ddd
Darfune <harolep@gmail.com>
2022-07-13 20:32:11 +0300

    Location Item file

--- app/src/main/java/com/example/jetpackweatherforecastapp/models/location/LocationItem.kt
+++ app/src/main/java/com/example/jetpackweatherforecastapp/models/location/LocationItem.kt
@@ -2,7 +2,7 @@ package com.example.jetpackweatherforecastapp.models.location
 
 data class LocationItem(
     val country: String,
-    val lat: Double,
+    var lat: Double,
     val lon: Double,
     val name: String,
     val state: String
