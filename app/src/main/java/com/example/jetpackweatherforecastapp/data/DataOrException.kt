283262ffbaf16c0263c0a0eabe0ba3988fec569e
Darfune <harolep@gmail.com>
2022-07-13 20:32:58 +0300

    Data or Exception class for the package calls

---
+++ app/src/main/java/com/example/jetpackweatherforecastapp/data/DataOrException.kt
@@ -0,0 +1,6 @@
+package com.example.jetpackweatherforecastapp.data
+
+class DataOrException<T, Boolean, E: Exception> (
+    var data: T? = null,
+    var loading: Boolean? = null,
+    var e: E? = null)
