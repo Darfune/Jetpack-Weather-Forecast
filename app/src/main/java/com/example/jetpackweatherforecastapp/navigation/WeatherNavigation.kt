382f5311df4e570b1dbdf7cd7254cab3af3ed52f
Darfune <harolep@gmail.com>
2022-07-13 20:29:41 +0300

    App navigation function

--- app/src/main/java/com/example/jetpackweatherforecastapp/navigation/WeatherNavigation.kt
+++ app/src/main/java/com/example/jetpackweatherforecastapp/navigation/WeatherNavigation.kt
@@ -1,10 +1,12 @@
 package com.example.jetpackweatherforecastapp.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.jetpackweatherforecastapp.screens.MainScreen
+import com.example.jetpackweatherforecastapp.screens.main.MainViewModel
 import com.example.jetpackweatherforecastapp.screens.splash.WeatherSplashScreen
 
 @Composable
@@ -17,7 +19,8 @@ fun WeatherNavigation() {
         }
 
         composable(WeatherScreens.MainScreen.name){
-            MainScreen(navController = navController)
+            val mainViewModel = hiltViewModel<MainViewModel>()
+            MainScreen(navController = navController, mainViewModel)
         }
     }
 }
