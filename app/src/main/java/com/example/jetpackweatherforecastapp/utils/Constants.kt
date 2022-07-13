6963e5272e14ba6fcd9b1ea3c4e06023bed1509f
Darfune <harolep@gmail.com>
2022-07-13 20:28:08 +0300

    File with the Constants

--- app/src/main/java/com/example/jetpackweatherforecastapp/utils/Constants.kt
+++ app/src/main/java/com/example/jetpackweatherforecastapp/utils/Constants.kt
@@ -3,6 +3,6 @@ package com.example.jetpackweatherforecastapp.utils
 import com.example.jetpackweatherforecastapp.BuildConfig
 
 object Constants {
-    const val BASE_URL = "http://api.openweathermap.org"
+    const val BASE_URL = "https://api.openweathermap.org"
     const val API_KEY = BuildConfig.API_KEY
 }
