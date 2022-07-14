5de73ef7eb99723f4d2a27cc668e103ff0fd7b55
Darfune <harolep@gmail.com>
2022-07-14 18:08:58 +0300

    Utils file for formatting data

--- app/src/main/java/com/example/jetpackweatherforecastapp/utils/Utils.kt
+++ app/src/main/java/com/example/jetpackweatherforecastapp/utils/Utils.kt
@@ -12,3 +12,14 @@ fun formatData(timestamp: Int): String {
     return sdf.format(date)
 }
 
+@SuppressLint("SimpleDateFormat")
+fun formatDateTime(timestamp: Int): String {
+    val sdf = SimpleDateFormat("hh:mm:aa")
+    val date = Date(timestamp.toLong() * 1000)
+
+    return sdf.format(date)
+}
+
+fun formatDecimals(item: Double): String {
+    return " %.0f".format(item)
+}
