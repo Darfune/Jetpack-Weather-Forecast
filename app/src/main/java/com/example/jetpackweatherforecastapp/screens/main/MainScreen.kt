d748aed5717de9e1f314b6024790cc3fa1035b05
Darfune <harolep@gmail.com>
2022-07-16 17:31:58 +0300

    added 5-day forecast

--- app/src/main/java/com/example/jetpackweatherforecastapp/screens/main/MainScreen.kt
+++ app/src/main/java/com/example/jetpackweatherforecastapp/screens/main/MainScreen.kt
@@ -2,7 +2,11 @@ package com.example.jetpackweatherforecastapp.screens.main
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.produceState
@@ -10,8 +14,12 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
@@ -76,7 +84,7 @@ fun MainContent(data: Weather) {
     verticalArrangement = Arrangement.Center,
     horizontalAlignment = Alignment.CenterHorizontally) {
 
-        Text(text = formatData(currentWeather.dt),
+        Text(text = formatData(currentWeather.dt).split(".")[0],
             style = MaterialTheme.typography.caption,
             color = MaterialTheme.colors.onSecondary,
             fontWeight = FontWeight.SemiBold,
@@ -102,6 +110,67 @@ fun MainContent(data: Weather) {
         HumidityWindPressureRow(currentWeather)
         Divider()
         SunriseAndSunsetRow(data)
+        Text(text = "This Week",
+            style = MaterialTheme.typography.subtitle1,
+            fontWeight = FontWeight.Bold)
+
+        Surface(modifier = Modifier
+            .fillMaxSize(),
+        color = MaterialTheme.colors.surface,
+        shape = RoundedCornerShape(size = 14.dp)) {
+            LazyColumn(modifier = Modifier.padding(2.dp),
+            contentPadding = PaddingValues(1.dp)){
+                items(items = data.list ) { item: WeatherItem ->  
+//                    Text(item.main.temp_max.toString())
+                    WeatherDetailRow(weather = item)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun WeatherDetailRow(weather: WeatherItem) {
+
+    val imageUrl = "https://openweathermap.org/img/wn/${weather.weather[0].icon}.png"
+
+    Surface(
+        Modifier
+            .padding(3.dp)
+            .fillMaxWidth(),
+        shape = CircleShape.copy(topEnd = CornerSize(6.dp)),
+        color = MaterialTheme.colors.primary) {
+
+        Row(modifier = Modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween) {
+            Text(formatData(weather.dt)
+                .split(",")[0] + formatData(weather.dt)
+                .split(".")[1],
+            modifier = Modifier.padding(start = 5.dp))
+            WeatherStateImage(imageUrl = imageUrl)
+            Surface(modifier = Modifier.padding(0.dp),
+            shape = CircleShape,
+            color = MaterialTheme.colors.secondaryVariant) {
+                Text(weather.weather[0].description,
+                    modifier = Modifier.padding(4.dp),
+                    style = MaterialTheme.typography.caption)
+            }
+            Text(text = buildAnnotatedString {
+                withStyle(style = SpanStyle(
+                    color = Color.Blue.copy(alpha = 0.7f),
+                    fontWeight = FontWeight.SemiBold
+                )){
+                    append(formatDecimals(weather.main.temp_max) + "°")
+                }
+                withStyle(style = SpanStyle(
+                    color = Color.LightGray
+                )
+                ){
+                    append(formatDecimals(weather.main.temp_min) + "°")
+                }
+            })
+        }
     }
 }
 
