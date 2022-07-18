6e8ca7953fd5b02c328e0399b0cda861706b2d78
Darfune <harolep@gmail.com>
2022-07-18 17:28:23 +0300

    Dropdown Options init

--- app/src/main/java/com/example/jetpackweatherforecastapp/widgets/WeatherAppBar.kt
+++ app/src/main/java/com/example/jetpackweatherforecastapp/widgets/WeatherAppBar.kt
@@ -1,12 +1,18 @@
 package com.example.jetpackweatherforecastapp.widgets
 
 import android.graphics.drawable.Icon
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material.icons.filled.Info
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material.icons.rounded.MoreVert
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
@@ -20,6 +26,7 @@ import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 
 @Composable
 fun WeatherAppBar(
@@ -30,6 +37,15 @@ fun WeatherAppBar(
     navController: NavController,
     onAddActionClicked: () -> Unit = {},
     onButtonClicked: () -> Unit = {}){
+
+    val showDialog = remember {
+        mutableStateOf(false)
+    }
+
+    if (showDialog.value) {
+        SHowSettingDropMenu(showDialog = showDialog, navController = navController)
+    }
+
 
     TopAppBar (title = {
                        Text(text = title,
@@ -44,7 +60,9 @@ fun WeatherAppBar(
                       }) {
                           Icon(imageVector = Icons.Default.Search, contentDescription = "Search icons")
                       }
-                      IconButton(onClick = { /*TODO*/ }) {
+                      IconButton(onClick = {
+                          showDialog.value = true
+                      }) {
                           Icon(imageVector = Icons.Rounded.MoreVert , contentDescription = "More Icons")
                       }
                   } else { }
@@ -61,4 +79,53 @@ fun WeatherAppBar(
         backgroundColor = Color.Transparent,
         elevation = elevation)
 
+}
+
+@Composable
+fun SHowSettingDropMenu(showDialog: MutableState<Boolean>, navController: NavController) {
+
+    var expanded by remember {
+        mutableStateOf(true)
+    }
+
+    val options = listOf("About", "Favorites", "Settings")
+
+    Column(modifier = Modifier
+        .fillMaxWidth()
+        .wrapContentSize(Alignment.TopEnd)
+        .absolutePadding(top = 45.dp, right = 20.dp)) {
+        DropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false },
+        modifier = Modifier
+            .width(140.dp)
+            .background(MaterialTheme.colors.background)) {
+            options.forEachIndexed { index, text ->
+                DropdownMenuItem(onClick = {
+                    expanded = false
+                    showDialog.value = false
+                }) {
+                    Icon(imageVector =
+                    when(text) {
+                        "About" -> Icons.Default.Info
+                        "Favorites" -> Icons.Default.FavoriteBorder
+                        else -> Icons.Default.Settings
+                                                  },
+                        contentDescription = null,
+                        tint = Color.LightGray)
+                    
+                    Text(text = text, modifier = Modifier.clickable {
+
+                    }, fontWeight = FontWeight.W300)
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewTopAppBar() {
+    val navController = rememberNavController()
+    WeatherAppBar(navController = navController)
 }
